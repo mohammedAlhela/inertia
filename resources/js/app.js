e75d89bdf6de537f0ce5fa34cb1e77234047ae82
Mohammed alhelal <jackij290@gmail.com>
2023-01-05 19:17:34 +0400

    handling inertia

---
+++ resources/js/app.js
@@ -0,0 +1,17 @@
+require('./bootstrap');
+import Vue from 'vue'
+import { createInertiaApp } from '@inertiajs/inertia-vue'
+import { InertiaProgress } from '@inertiajs/progress'
+
+InertiaProgress.init()
+
+createInertiaApp({
+  resolve: name => import(`./Pages/${name}`),
+  setup({ el, App, props, plugin }) {
+    Vue.use(plugin)
+
+    new Vue({
+      render: h => h(App, props),
+    }).$mount(el)
+  },
+})
